2b0f084734ada4b9f95a3228b37a154e077cb44c
Lukas Willin <lukas.willin@vtxmail.ch>
2022-12-18 20:14:08 +0100

    dist/static from 0.1.0

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="m-3">
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="./">Home</router-link> |
+    <router-link to="./about">About</router-link>
   </nav>
   <router-view />
 </div>
